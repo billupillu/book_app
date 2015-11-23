e27b23954f69ccc1f2dc2b510b1e78302ef44db8
sumeet <sumeet@sumi-pc.(none)>
2015-11-23 13:55:33 +0530

    author_spec edit 1

---
+++ spec/models/author_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Author, type: :model do
+
+  it "should have a first_name"
+
+  it "should have a second_name"
+
+  describe "should return a full_name" do
+
+  end
+
+end
