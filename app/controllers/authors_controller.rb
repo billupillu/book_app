1e547bd0c2351d01a27066743018d4825961efcd
Sumeet Masih <billu.pillu123@gmail.com>
2015-11-26 22:05:35 +0530

    author controller test complete

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,10 +1,11 @@
 class AuthorsController < ApplicationController
+  before_action :find_author, only: [:show, :edit, :update, :destroy]
+
   def index
     @authors = Author.recent
   end
 
   def show
-    @author = Author.find(params[:id])
   end
 
   def new
@@ -22,8 +23,30 @@ class AuthorsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @author.update(author_params)
+      flash[:success] = "Author has been successfuly Updated"
+      redirect_to @author
+    else
+      flash[:danger] = "Author has not been Updated"
+      render :edit
+    end
+  end
+
+  def destroy
+    @author.destroy
+    flash[:success] = "Author has been successfuly Deleted"
+    redirect_to authors_path
+  end
 
   private
+
+    def find_author
+      @author = Author.find(params[:id])
+    end
 
     def author_params
       params.require(:author).permit(:first_name, :second_name)
--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -52,4 +52,61 @@ RSpec.describe AuthorsController, type: :controller do
     end
   end
 
+  describe "GET #edit" do
+    it "successfully renders the edit template" do
+      author = Fabricate(:author)
+      get :edit, id: author.id
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "PUT #update" do
+    context "Successful Update" do
+      let(:author) {Fabricate(:author, first_name: "Chill")}
+
+      it "to successfuly update the author" do
+        put :update, author: Fabricate.attributes_for(:author, first_name: "Bill"), id: author.id
+        expect(Author.last.first_name).to eq("Bill")
+        expect(Author.last.first_name).not_to eq("Chill")
+      end
+      it "displays success flash" do
+        put :update, author: Fabricate.attributes_for(:author, first_name: "Bill"), id: author.id
+        expect(flash[:success]).to eq("Author has been successfuly Updated")
+      end
+      it "redirects to show" do
+        put :update, author: Fabricate.attributes_for(:author, first_name: "Bill"), id: author.id
+        expect(response).to redirect_to(author_path(Author.last))
+      end
+    end
+
+    context "Unsuccessful Update" do
+      let(:author) {Fabricate(:author, first_name: "Bill")}
+
+      it "does not update author for invalid input" do
+        put :update, author: Fabricate.attributes_for(:author, first_name: nil), id: author.id
+        expect(Author.last.first_name).to eq("Bill")
+      end
+      it "displays danger flash" do
+        put :update, author: Fabricate.attributes_for(:author, first_name: nil), id: author.id
+        expect(flash[:danger]).to eq("Author has not been Updated")
+      end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    let(:author) {Fabricate(:author)}
+
+    it "successfuly deletes the author" do
+      delete :destroy, id: author.id
+      expect(Author.count).to eq(0)
+    end
+    it "display success flash for deletion" do
+      delete :destroy, id: author.id
+      expect(flash[:success]).to eq("Author has been successfuly Deleted")
+    end
+    it "redirects to index action" do
+      delete :destroy, id: author.id
+      expect(response).to redirect_to(authors_path)
+    end
+  end
 end
