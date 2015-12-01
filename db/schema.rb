85cc88dfd4674822ee0dce76f4fffff634ddfbfd
Sumeet Masih <billu.pillu123@gmail.com>
2015-12-01 13:11:56 +0530

    adding an admin field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151129201136) do
+ActiveRecord::Schema.define(version: 20151201073859) do
 
   create_table "athors", force: :cascade do |t|
     t.string   "first_name"
@@ -61,8 +61,9 @@ ActiveRecord::Schema.define(version: 20151129201136) do
     t.string   "username",        limit: 100
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
+    t.datetime "created_at",                                  null: false
+    t.datetime "updated_at",                                  null: false
+    t.boolean  "admin",                       default: false
   end
 
 end
--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -2,4 +2,9 @@ Fabricator(:user) do
 	username { Faker::Internet.user_name }
 	email {Faker::Internet.email}
 	password {Faker::Internet.password(8)}
+	admin false
+end
+
+Fabricator(:admin, from: :user) do
+	admin true
 end
